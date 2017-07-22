9f94b3c58176e838f8fbf9bb7b5210568369b0cd
tr0bins0n <robinson.te@gmail.com>
2017-07-22 18:10:00 -0500

    Add files via upload

---
+++ github_test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 22 17:59:21 2017
+
+@author: robin
+"""
+
+
+def githubtest_func(key):
+    if key == 1:
+        print('This is a function for Hello World to github')
+    else:
+        print('This is STILL a function for Hello World to github')
+            
+def main(key):
+    githubtest_func(key)
+    
+main(2)
